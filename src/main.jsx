73561f9b45f9773a7e5e3bd68c489ef1e1f94dd8
Максим <128375823+Rimpect@users.noreply.github.com>
2025-02-03 18:40:27 +0300

    Update main.jsx
    StrictMode узнать

--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,7 @@ import App from './App.jsx'
 import './index.css'
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
+  <StrictMode> // зачем StrictMode нужен
     <App />
   </StrictMode>,
 )
